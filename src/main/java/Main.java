2f54f7c71577df6eb7272fcdbc795a0ad9f4ec09
zerolijintao <13636655697@163.com>
2020-02-14 12:13:45 +0800

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        
         ReentrantLock lock = new ReentrantLock(true);
         lock.lock();
         System.out.println("go");
